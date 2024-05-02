b8bc2a02d036a84ffcbeb087285c5c58cf58ccca
ashish <ashish@skyserve.ai>
2024-05-02 14:41:20 +0530

    add infinite scroll logic for calling API

--- client/src/services/getJobLists.js
+++ client/src/services/getJobLists.js
@@ -1,17 +1,23 @@
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+export async function getJobLists(limit, offset) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-const raw = JSON.stringify({
- "limit": 10,
- "offset": 0
-});
+    const raw = JSON.stringify({
+        "limit": 10,
+        "offset": 20
+    });
 
-const requestOptions = {
- method: "POST",
- headers: myHeaders,
-};
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+    };
 
-fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
- .then((response) => response.text())
- .then((result) => console.log(result))
- .catch((error) => console.error(error));
+    try {
+        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
